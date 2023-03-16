f939f0126ae255836c2e6989942246d5e1ab1f83
Ayush <snglayush@gmail.com>
2023-03-16 22:15:46 +0530

    Sign-up page setted up with database and checks

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,46 @@
+const User = require('../models/user');
 module.exports.profile = function(req,res){
   //return res.end('<h1> user profile </h1>');
 
   return res.render('user_profile',{
     title: "profile!"
   });
+}
+
+// render sign-up page
+module.exports.signUp= function(req,res){
+  return res.render('user_sign_up',{
+    title: "Etalk | Sign up "
+  });
+}
+
+module.exports.signIn = function(req,res){
+  return res.render('user_sign_in',{
+    title: "Etalk | Sign In"
+  });
+}
+
+// get the sign-up data
+module.exports.create = async function(req,res){
+
+if(req.body.password != req.body.confirm_password){
+  return res.redirect('back');
+}
+try {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    await User.create(req.body);
+    return res.redirect('/users/sign-in');
+  } else {
+    return res.redirect('back');
+  }
+} catch (err) {
+  console.log('error in finding user in signing up', err);
+  return;
+}
+
+}
+
+module.exports.createSession = function(req,res){
+
 }
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
@@ -22,4 +27,4 @@ app.listen(port,function(err){
     console.log(`erro : ${err}`);
   }
 console.log(`server is running on port ${port} ! `);
-})
+});
