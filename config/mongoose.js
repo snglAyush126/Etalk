3afbf0f73d867ab245b9bf1b1a26ef9f6c5ba20f
snglAyush126 <snglayush@gmail.com>
2024-07-03 16:26:59 +0530

    Updating MongoDB file adding SSL

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,14 +1,29 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/Etalk`);
-const db= mongoose.connection;
+const dbUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/Etalk`;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  sslValidate: false, // Disable certificate validation (not recommended for production)
+};
 
-db.on('error',console.error.bind(console,"Error connectiing to MongoDB"));
-
-db.once('open',function(){
-
-
+mongoose.connect(dbUri, options).then(() => {
   console.log('Connected to database: MongoDB');
-
+}).catch(err => {
+  console.error('Error connecting to MongoDB', err);
+  console.log('Connection options:', options);
+  console.log('Database URI:', dbUri);
 });
-module.exports= db;
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
+
+db.once('open', function() {
+  console.log('Connected to database: MongoDB');
+});
+
+module.exports = db;
+
