424e34cd2601d1c5fdde92fbadd72e85d621220b
snglAyush126 <snglayush@gmail.com>
2024-07-03 18:49:12 +0530

    Updating io Port

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -5,7 +5,7 @@ class ChatEngine
     {
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
-        this.socket = io.connect('http://localhost:5000');
+        this.socket = io.connect(`${process.env.CHAT_PORT}`);
 
         if (this.userEmail)
         {
