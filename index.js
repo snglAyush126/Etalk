1ac38337be9c1e2e7aad16af882ab1253c97405d
snglAyush126 <snglayush@gmail.com>
2024-07-03 15:48:40 +0530

    updated env PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const session = require('express-session');
